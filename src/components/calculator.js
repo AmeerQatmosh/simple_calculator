8c8f5519d02993be7ebeaca73a95d425874e1b3b
Ameer Qatmosh <ameerqsh@gmail.com>
2023-05-24 16:47:21 +0300

    replace class with function

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,69 +1,66 @@
-import React from 'react';
 import './calculator.css'
+import React, { useState } from 'react';
 
-export default class Calculator extends React.Component{
-    constructor(props){
-        super(props);
-        this.state={primary:0,secondary:0};
-    }
-    sum =() =>{
-        var N1 = parseInt(this.state.primary);
-        var N2 = parseInt(this.state.secondary);
-        var Results = N1+N2;
-        this.setState({myText:Results})
-    }
-    subtract =() =>{
-        var N1 = parseInt(this.state.primary);
-        var N2 = parseInt(this.state.secondary);
-        var Results = N1-N2;
-        this.setState({myText:Results})
-    }
-    multiply =() =>{
-        var N1 = parseInt(this.state.primary);
-        var N2 = parseInt(this.state.secondary);
-        var Results = N1*N2;
-        this.setState({myText:Results})
-    }
-    devide =() =>{
-        
-        var N1 = parseInt(this.state.primary);
-        var N2 = parseInt(this.state.secondary);
-        if (N2===0){
-            const error = "Error: Can't Devide by zero!";
-            this.setState({myText:error})
-        }
-        else {
-            var Results = parseInt(N1/N2);
-            this.setState({myText:Results})
-        }
-    }
-
-    render(){
-        return(
-            <div>
-                <span>
-                    <form>
-                        <h1>Simple Calculator App</h1>
-                        <input placeholder='First Value' onChange={primary => this.setState({ primary: primary.target.value })} />
-                        <input placeholder='Second Value' onChange={secondary => this.setState({ secondary: secondary.target.value })} />
-                    </form>
-                </span>
-
-                <p>Just Enter The values and click on the wanted Operation button and the result will immediately be shown. </p>
-
-
-                <div>
-                    <label>Result: {this.state.myText}</label>
-                 </div>
-
-                <div>
-                    <button onClick={this.sum}>+</button>
-                    <button onClick={this.subtract}>-</button>
-                    <button onClick={this.multiply}>×</button>
-                    <button onClick={this.devide}>÷</button>
-                </div>
-    
+export default function Calculator() {
+    const [primary, setPrimary] = useState(0);
+    const [secondary, setSecondary] = useState(0);
+    const [myText, setMyText] = useState(0);
+  
+    const sum = () => {
+      let N1 = parseFloat(primary);
+      let N2 = parseFloat(secondary);
+      let Results = N1 + N2;
+      setMyText(Results);
+    };
+  
+    const subtract = () => {
+      let N1 = parseFloat(primary);
+      let N2 = parseFloat(secondary);
+      let Results = N1 - N2;
+      setMyText(Results);
+    };
+  
+    const multiply = () => {
+      let N1 = parseFloat(primary);
+      let N2 = parseFloat(secondary);
+      let Results = N1 * N2;
+      setMyText(Results);
+    };
+  
+    const divide = () => {
+      let N1 = parseFloat(primary);
+      let N2 = parseFloat(secondary);
+      if (N2 === 0) {
+        let error = "Error: Can't Divide by zero!";
+        setMyText(error);
+      } else {
+        let Results = parseFloat(N1 / N2);
+        setMyText(Results);
+      }
+    };
+  
+    return (
+      <div>
+        <span>
+          <form>
+            <h1>Simple Calculator App</h1>
+            <input placeholder='First Value' onChange={e => setPrimary(e.target.value)} />
+            <input placeholder='Second Value' onChange={e => setSecondary(e.target.value)} />
+          </form>
+        </span>
+  
+        <p>Just Enter The values and click on the wanted Operation button and the result will immediately be shown. </p>
+  
+        <div>
+          <label>Result: {myText}</label>
         </div>
-        );
-    }
-}
+  
+        <div>
+          <button onClick={sum}>+</button>
+          <button onClick={subtract}>-</button>
+          <button onClick={multiply}>×</button>
+          <button onClick={divide}>÷</button>
+        </div>
+      </div>
+    );
+  }
